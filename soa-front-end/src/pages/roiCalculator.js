62c47b7e9460c6e518a6afdfc1f3c4f2d8c40279
kevinli8221 <kevinli8221@gmail.com>
2023-11-28 15:40:22 -0500

    added pages

---
+++ soa-front-end/src/pages/roiCalculator.js
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+const roiCalculator = () => {
+    return (
+        <div>
+            <h1>
+                Roi Calculator Page
+            </h1>
+        </div>
+    );
+};
+ 
+export default roiCalculator;
