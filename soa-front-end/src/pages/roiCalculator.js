40e73fb4819c3a57087e17d6b7a146b8d34f8129
sgilhul <sgilhul@uwo.ca>
2023-12-03 18:39:03 -0500

    Graph updates properly now yay

--- soa-front-end/src/pages/roiCalculator.js
+++ soa-front-end/src/pages/roiCalculator.js
@@ -1,18 +1,31 @@
-import { React, useState } from "react";
+import { React, useState, useRef } from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 
 
-function roiCalculator() {
+function useRoiCalculator() {
 	
-	const doi = [[200, 300, 523, 123, 534, 308], [213, 25, 120, 320, 102, 503]];
-	const [data, setData] = useState([0,0,0,0,0,0]);
+	const doi = [[
+		{date: 1, value: 34},
+		{date: 2, value: 340},
+		{date: 3, value: 343},
+		{date: 4, value: 324},
+		{date: 5, value: 314},
+		{date: 6, value: 354}
+	],[
+		{date: 1, value: 34},
+		{date: 2, value: 540},
+		{date: 3, value: 2},
+		{date: 4, value: 31},
+		{date: 5, value: 100},
+		{date: 6, value: 60}
+	]];
+	const [data, setData] = useState(doi[0]);
 	
 	const handleStockChange = () => {
-		var drop = document.getElementById("stock");
-		var graph = document.getElementById("graph");
-		
-		setData(doi[drop.selectedIndex]);
+		setData(doi[stockRef.current.selectedIndex]);
 	}
+	
+	const stockRef = useRef(null);
 	
     return (
         <div id="body">
@@ -20,22 +33,25 @@ function roiCalculator() {
                 Roi Calculator Page
             </h1>
 			
-			<LineChart name="graph" id="graph"
+			<LineChart
 				width = {500}
 				height = {300}
-				series={[
-					{ data: data },
-				]}
-				xAxis={[{ scaleType: "point", data: [1,2,3,4,5,6] }]}
+				series={[{
+					dataKey: "value",
+				}]}
+				xAxis={[{ scaleType: "point", dataKey: "date" }]}
+				
+				dataset = {data}
 			>
 			</LineChart>
 			
-			<select name="stock" id="stock" onchange={handleStockChange}>
+			<select ref={stockRef} onChange={handleStockChange}>
 				<option value="DTA1">Data 1</option>
 				<option value="DTA2">Data 2</option>
 			</select>
+			
         </div>
     );
 };
  
-export default roiCalculator;
+export default useRoiCalculator;
