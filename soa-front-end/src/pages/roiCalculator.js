7a7387ed2a50b5c4f1553cb5e1d6fc64dc0834a2
sgilhul <sgilhul@uwo.ca>
2023-12-02 23:21:53 -0500

    Working on graph implementation

--- soa-front-end/src/pages/roiCalculator.js
+++ soa-front-end/src/pages/roiCalculator.js
@@ -1,11 +1,39 @@
-import React from "react";
- 
-const roiCalculator = () => {
+import { React, useState } from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+
+
+function roiCalculator() {
+	
+	const doi = [[200, 300, 523, 123, 534, 308], [213, 25, 120, 320, 102, 503]];
+	const [data, setData] = useState([0,0,0,0,0,0]);
+	
+	const handleStockChange = () => {
+		var drop = document.getElementById("stock");
+		var graph = document.getElementById("graph");
+		
+		setData(doi[drop.selectedIndex]);
+	}
+	
     return (
-        <div>
+        <div id="body">
             <h1>
                 Roi Calculator Page
             </h1>
+			
+			<LineChart name="graph" id="graph"
+				width = {500}
+				height = {300}
+				series={[
+					{ data: data },
+				]}
+				xAxis={[{ scaleType: "point", data: [1,2,3,4,5,6] }]}
+			>
+			</LineChart>
+			
+			<select name="stock" id="stock" onchange={handleStockChange}>
+				<option value="DTA1">Data 1</option>
+				<option value="DTA2">Data 2</option>
+			</select>
         </div>
     );
 };
